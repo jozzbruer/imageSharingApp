c969cda696d9b37858f6a24f3958217ba32376cf
Joz-Bruer Quince <quincejozbruer@gmail.com>
2023-12-08 19:51:24 -0800

    Add the textfield and an upload button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
-
 import './App.css';
+import { Box, Button, Container, TextField } from '@mui/material';
 
-const App: React.FC =() => {
-  return (
-    <>
-      Hello test
-    </>
-  );
-}
+const App: React.FC = () => {
+	return (
+		<Container>
+			<Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+				<TextField
+					label='Search'
+					sx={{ width: '60%' }}
+					variant='outlined'
+					margin='normal'
+				/>
+				<Button
+					sx={{ marginTop: '15px', marginBottom: '10px' }}
+					variant='contained'
+					color='primary'
+					component='label'>
+					Upload
+					<input
+						type='file'
+						hidden
+					/>
+				</Button>
+			</Box>
+		</Container>
+	);
+};
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,15 +4,11 @@ import './index.css';
 import App from './App';
 import { theme } from './theme/theme';
 import { ThemeProvider } from '@emotion/react';
-import { CssBaseline } from '@mui/material';
 
-const root = ReactDOM.createRoot(
-	document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
-      <CssBaseline />
 			<App />
 		</ThemeProvider>
 	</React.StrictMode>
